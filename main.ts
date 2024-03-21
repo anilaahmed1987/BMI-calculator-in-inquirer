4513c75b2178dcc0e92657e57de871e9fd9b79f9
anilaaahmed <anilaahmed1987@gmail.com>
2024-03-22 04:39:13 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,16 @@
+// BMI calculator using inquirer prompt
+import inquirer from "inquirer";
+const question =[
+    {type:"number",name:"weight",message:"weight(kg):"},
+    {type:"number", name:"height",message:"height(foot):"},
+]
+    const calculateBMI=(weight:number,height:number):number=>weight/(height*height);
+    const main =async() =>{
+        const{weight,height}=await inquirer.prompt(question)
+        console.log(`BMI: ${calculateBMI(weight,height) .toFixed(2)}`)
+    }
+        main();
+    
+
+
+
